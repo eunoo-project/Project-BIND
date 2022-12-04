a8b8950cdab64356db3f6947341d4dbcfcdb8f52
eunoo1995 <eunoo1995@gmail.com>
2022-12-04 18:59:34 +0900

    feat. inplement UserProfile component
    유저 프로필 컴포넌트 구현
    서버에서 이미지를 받아와 보여주는 것은 서버 개발이 완료된 후 구현할 예정

---
+++ client/src/components/UserProfile/UserProfile.tsx
@@ -0,0 +1,31 @@
+import SmallProfile from '@/../public/assets/profile-small.svg';
+import BigProfile from '@/../public/assets/profile-big.svg';
+import styles from './UserProfile.module.css';
+import classNames from 'classnames';
+import Image from 'next/image';
+
+interface UserProfileProps {
+  size?: 'big' | 'small';
+  id: string;
+  img?: string;
+  // 해당 유저 페이지로 이동하는 함수
+  onClick?: () => void;
+  [key: string]: unknown;
+}
+
+//prettier-ignore
+export const UserProfile = ({ id, size, img, ...props }: UserProfileProps) => (
+  <button className={classNames(styles.button, size === 'big' && 'flex-col') } {...props}>
+    {size === 'big' ? (
+      img ?  '이미지' : <BigProfile fill="black" />
+    ) : (
+      img ?  '이미지' : <SmallProfile fill="black" />
+    )}
+    <span className={styles.id}>{id}</span>
+  </button>
+);
+
+UserProfile.defaultProps = {
+  id: 'empty',
+  size: 'small',
+};
--- client/src/components/index.tsx
+++ client/src/components/index.tsx
@@ -2,3 +2,4 @@ export * from './A11yHidden/A11yHidden';
 export * from './ErrorBoundary/ErrorBoundary';
 export * from './Button/Button';
 export * from './Message/Message';
+export * from './UserProfile/UserProfile';
