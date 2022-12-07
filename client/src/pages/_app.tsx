c7ecee5d52cbeed7c25e94d4eebbf7139451f508
eunoo1995 <eunoo1995@gmail.com>
2022-12-07 18:31:23 +0900

    test. test how to use recoil
    리코일 라이브러리 사용법에 대한 테스트 진행

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ErrorBoundary } from '@/components';
+import { RecoilRoot } from 'recoil';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ErrorBoundary>
-      <Component {...pageProps} />
-    </ErrorBoundary>
+    <RecoilRoot>
+      <ErrorBoundary>
+        <Component {...pageProps} />
+      </ErrorBoundary>
+    </RecoilRoot>
   );
 }
