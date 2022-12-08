6d352f3ff67a42e3fedabbaf084914c93a3a5737
eunoo1995 <eunoo1995@gmail.com>
2022-12-08 21:17:09 +0900

    feat. implement RegisterForm component
    회원가입 폼 구현

---
+++ client/src/containers/RegisterForm/RegisterForm.stories.tsx
@@ -0,0 +1,13 @@
+import { RegisterForm } from '@/containers';
+// import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+export default {
+  title: 'containers/RegisterFrom',
+  component: RegisterForm,
+};
+
+// 템플릿 생성
+const Template = () => <RegisterForm />;
+
+// default
+export const Default = Template.bind({});
