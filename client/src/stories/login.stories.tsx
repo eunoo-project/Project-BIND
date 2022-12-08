cb5eb622c9f8b054789589e5e1c6232b1bdeae3b
eunoo1995 <eunoo1995@gmail.com>
2022-12-08 20:07:42 +0900

    feat. implement Login Page
    로그인 페이지 구현 및 테스트

---
+++ client/src/stories/login.stories.tsx
@@ -0,0 +1,14 @@
+import Login from '@/pages/login';
+// import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+export default {
+  title: 'pages/Login',
+  component: Login,
+  parameters: {},
+};
+
+// 템플릿 생성
+const Template = () => <Login />;
+
+// default
+export const Default = Template.bind({});
