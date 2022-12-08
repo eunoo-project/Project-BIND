7c8605ca94d41223741f26f381a109d25d8f86ce
eunoo1995 <eunoo1995@gmail.com>
2022-12-08 20:02:20 +0900

    feat. add common head tag
    공통 헤더 태그 추가

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <meta name="description" content="친구와 BIND로 묶이다!" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
