1ee0489c52d17b6d11a2bdab177805040ea4e710
eunoo1995 <eunoo1995@gmail.com>
2022-12-08 21:17:40 +0900

    fix. modify validate condition
    유효성 검사 버그 수정

--- client/src/utils/validate.ts
+++ client/src/utils/validate.ts
@@ -9,15 +9,15 @@ export const isValidate = ({
   password,
   confirmPassword,
 }: isValidateProps) => {
-  if (!confirmPassword) {
+  if (confirmPassword !== undefined) {
     return !(
       /^[a-z][a-z0-9_]{4,11}$/.test(userId) &&
-      /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,20}$/.test(password)
+      /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,20}$/.test(password) &&
+      password === confirmPassword
     );
   }
   return !(
     /^[a-z][a-z0-9_]{4,11}$/.test(userId) &&
-    /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,20}$/.test(password) &&
-    password === confirmPassword
+    /^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,20}$/.test(password)
   );
 };
