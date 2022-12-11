0f4f22b5f8fcde705d7a7eab5f8a97140b49d7a8
eunoo1995 <eunoo1995@gmail.com>
2022-12-11 16:21:57 +0900

    feat. add QueryClientProvider at root
    리액트 쿼리를 사용하기 위한 프로바이더 추가

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -2,13 +2,21 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ErrorBoundary } from '@/components';
 import { RecoilRoot } from 'recoil';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
+// import { Suspense } from 'react';
+
+const queryclient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <RecoilRoot>
-      <ErrorBoundary>
-        <Component {...pageProps} />
-      </ErrorBoundary>
+      <QueryClientProvider client={queryclient}>
+        {/* <Suspense fallback="로딩중..."> */}
+        <ErrorBoundary>
+          <Component {...pageProps} />
+        </ErrorBoundary>
+        {/* </Suspense> */}
+      </QueryClientProvider>
     </RecoilRoot>
   );
 }
