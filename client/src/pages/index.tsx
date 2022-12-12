d8a0c86a25973bfbcd43b96e43c2b3b02635afa7
eunoo1995 <eunoo1995@gmail.com>
2022-12-12 20:49:42 +0900

    feat. implement Main Page (...ing)
    메인 페이지 개발 (진행중)

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,14 +1,26 @@
 import Head from 'next/head';
-import Link from 'next/link';
 import styles from '@/styles/main.module.css';
 import { Nav, Header } from '@/layout';
+import { Post } from '@/containers';
+import { usePosts } from '@/hooks/chat';
+import { postProps } from '@/containers';
 
 const Main = () => {
+  const { data: posts } = usePosts();
   return (
     <>
+      <Head>
+        <title>BIND</title>
+      </Head>
       <Header />
       <Nav />
-      <main className={styles.main} />
+      <main className={styles.main}>
+        <ul className={styles.container}>
+          {posts?.map((post: postProps) => (
+            <Post key={post._id} post={post} />
+          ))}
+        </ul>
+      </main>
     </>
   );
 };
