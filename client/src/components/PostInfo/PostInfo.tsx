e20898190ee6891f1a3bf104d7a87d23011be630
eunoo1995 <eunoo1995@gmail.com>
2022-12-12 17:43:14 +0900

    feat. implement PostInfo component
    포스트 정보 컴포넌트 구현

---
+++ client/src/components/PostInfo/PostInfo.tsx
@@ -0,0 +1,32 @@
+import Heart from '@/../public/assets/heart.svg';
+import HeartFill from '@/../public/assets/heart-fill.svg';
+import styles from './PostInfo.module.css';
+
+interface PostInfoProps {
+  isLikePost: boolean;
+  likeCnt: string;
+  description: string;
+  publishDate: string;
+  handleLike: () => void;
+}
+
+export const PostInfo = ({
+  isLikePost,
+  likeCnt,
+  description,
+  publishDate,
+  handleLike,
+}: PostInfoProps) => {
+  return (
+    <>
+      <div className={styles.likeInfo}>
+        <button onClick={handleLike}>
+          {isLikePost ? <HeartFill /> : <Heart />}
+        </button>
+        <span>좋아요 {likeCnt}개</span>
+      </div>
+      <p className={styles.description}>{description}</p>
+      <p className={styles.date}>{publishDate}</p>
+    </>
+  );
+};
