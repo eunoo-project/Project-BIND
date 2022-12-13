10aa7a19d1ef6ccf7f98982a3a7b1285140dc474
eunoo1995 <eunoo1995@gmail.com>
2022-12-13 21:53:03 +0900

    fix. userstate default value type error
    유저 전역 상태 기본값 타입 에러 해결

--- client/src/states/index.tsx
+++ client/src/states/index.tsx
@@ -2,7 +2,11 @@ import { atom } from 'recoil';
 
 export const userState = atom({
   key: 'userState',
-  default: null,
+  default: {
+    _id: null as string | null,
+    userId: null as string | null,
+    imageURL: null as string | null,
+  },
 });
 
 export const themeState = atom({
