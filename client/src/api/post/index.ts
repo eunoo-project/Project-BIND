913233c4386dea71f1e26b67d5d08aa42bb0e208
eunoo1995 <eunoo1995@gmail.com>
2022-12-13 21:33:27 +0900

    feat. add post api
    포스트 관련 api 추가

--- client/src/api/post/index.ts
+++ client/src/api/post/index.ts
@@ -6,3 +6,24 @@ export const getPosts = async () => {
   );
   return data;
 };
+
+export const updatePost = async ({
+  postId,
+  payload,
+}: {
+  postId: string;
+  payload: { [key: string]: unknown };
+}) => {
+  const { data } = await axios.patch(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/post/${postId}`,
+    { ...payload }
+  );
+  return data;
+};
+
+export const deletePost = async ({ postId }: { postId: string }) => {
+  const { data } = await axios.delete(
+    `${process.env.NEXT_PUBLIC_SERVER_URL}/post/${postId}`
+  );
+  return data;
+};
