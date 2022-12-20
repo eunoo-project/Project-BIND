6bd1cc2317fbe95b47fccb5ff22db5fda5ee9248
eunoo1995 <eunoo1995@gmail.com>
2022-12-20 17:02:58 +0900

    feat. implement ChatForm component
    채팅 폼 구현

--- client/src/containers/ChatForm/ChatForm.tsx
+++ client/src/containers/ChatForm/ChatForm.tsx
@@ -1,1 +1,41 @@
-export {};
+import styles from './ChatForm.module.css';
+import { Send } from '@/components';
+import { useState } from 'react';
+
+interface ChatFormProps {
+  submit: (message: string) => void;
+}
+
+export const ChatForm = ({ submit }: ChatFormProps) => {
+  const [message, setMessage] = useState('');
+
+  const handleChange = (e: React.ChangeEvent) => {
+    setMessage((e.target as HTMLInputElement).value);
+  };
+
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    submit(message);
+    setMessage('');
+  };
+
+  return (
+    <form className={styles.form} onSubmit={handleSubmit}>
+      <label className="sr-only" htmlFor="message">
+        메세지 입력창
+      </label>
+      <input
+        type="text"
+        className={styles.input}
+        value={message}
+        id="message"
+        autoComplete="off"
+        placeholder="메세지를 입력하세요..."
+        onChange={handleChange}
+      />
+      <button aria-label="전송" className={styles.button}>
+        <Send />
+      </button>
+    </form>
+  );
+};
