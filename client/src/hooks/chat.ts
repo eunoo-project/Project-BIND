11ad49d797a570c32ac31bcb74cd532b7dd56393
eunoo1995 <eunoo1995@gmail.com>
2022-12-20 16:57:42 +0900

    feat. add chat API and custom hooks
    채팅 api 와 훅 추가

--- client/src/hooks/chat.ts
+++ client/src/hooks/chat.ts
@@ -1,1 +1,19 @@
-export {};
+import { useQuery } from '@tanstack/react-query';
+import { getRooms, getRoom } from '@/api';
+
+export const useRooms = () => {
+  return useQuery(['rooms'], getRooms, {
+    retry: false,
+    staleTime: 3000,
+    cacheTime: Infinity,
+    // refetchInterval: 1500,
+    // refetchIntervalInBackground: true,
+  });
+};
+
+export const useRoom = (id: string) => {
+  return useQuery(['room', id], () => getRoom(id), {
+    retry: false,
+    refetchOnWindowFocus: false,
+  });
+};
