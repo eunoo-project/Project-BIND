2ef35ed36c512e91d7b0c42ac6142f7b12c2e56b
eunoo1995 <eunoo1995@gmail.com>
2022-12-20 17:02:24 +0900

    feat. implement RoomList component
    채팅방 목록 컴포넌트 구현

--- client/src/containers/RoomList/RoomList.tsx
+++ client/src/containers/RoomList/RoomList.tsx
@@ -1,1 +1,25 @@
-export {};
+import { useRooms } from '../../hooks/chat';
+import { Room, RoomProps } from '@/components';
+import styles from './RoomList.module.css';
+
+export const RoomList = () => {
+  const { data: rooms } = useRooms();
+
+  if (!rooms) return <></>;
+  return (
+    <>
+      {rooms?.length > 0 && rooms.every((room: RoomProps) => room?.lastChat) ? (
+        <ul className={styles.container}>
+          {rooms.map((room: RoomProps) => (
+            <Room key={room.roomId} {...room} />
+          ))}
+        </ul>
+      ) : (
+        <div className={styles.empty + ' h-full'}>
+          <p>BIND</p>
+          <p>대화중인 상대가 없습니다...</p>
+        </div>
+      )}
+    </>
+  );
+};
